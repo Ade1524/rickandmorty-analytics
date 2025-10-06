b30d180d6bd1423eb361c22f388798a08d2b8ef0
Adetayo Aruwajoye <adetayoajilo@gmail.com>
2025-10-06 07:50:26 +0000

    add the query for the analytical character questions

--- dbt_rickandmorty/macros/cast_columns.sql
+++ dbt_rickandmorty/macros/cast_columns.sql
@@ -1,12 +1,4 @@
-/*
-{% macro cast_columns(source_ref, column_types) %}
-    select
-    {% for col, dtype in column_types.items() %}
-        cast({{ col }} as {{ dtype }}) as {{ col }}{% if not loop.last %},{% endif %}
-    {% endfor %}
-    from {{ source_ref }}
-{% endmacro %}
-*/
+
 {% macro cast_and_rename(source_ref, column_map) %}
     select
     {% for col, props in column_map.items() %}

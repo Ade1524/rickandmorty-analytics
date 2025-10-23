89275e1334e95f089f52a18e5ab0f9b845b4b10b
Adetayo Aruwajoye <adetayoajilo@gmail.com>
2025-10-23 08:29:56 +0000

    Update the character questions

--- dbt_rickandmorty/analyses/analytical_questions/character_questions/species_survival_rate.sql
+++ dbt_rickandmorty/analyses/analytical_questions/character_questions/species_survival_rate.sql
@@ -1,27 +1,31 @@
 with species_status as (
     select
-        species,
-        status,
-        count(distinct dim_character_key) as character_count
-    from {{ ref('dim_character') }}
-    where status in ('Alive','Dead')
-    group by species, status
+        a.dim_character_key,
+        a.species,
+        a.status,
+        count(a.dim_character_key) as character_count
+    from {{ ref('dim_character') }} a
+    left join {{ ref('fct_character') }}  b on a.dim_character_key = b.dim_character_key
+    where a.status in ('Alive','Dead')
+    group by a.species, 
+             a.status,
+             a.dim_character_key
 ),
 
 aggregated as (
     select
-        species,
-        sum(case when status = 'Alive' then character_count else 0 end) as alive_count,
-        sum(case when status = 'Dead' then character_count else 0 end) as dead_count
-    from species_status
+        a.species,
+        sum(b.alive_count) as total_alive,
+        sum(b.death_count) as total_dead
+    from species_status a
+    left join {{ ref('fct_character') }}  b on a.dim_character_key = b.dim_character_key
     group by species
 )
 
 select
     species,
-    alive_count,
-    dead_count,
-    round(alive_count*1.0 / nullif(alive_count + dead_count,0),2) as survival_rate
+    total_alive,
+    total_dead,
+    round(total_alive*1.0 / nullif(total_alive + total_dead,0),2) as survival_rate
 from aggregated
 order by survival_rate desc
-
